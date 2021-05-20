7f23cdbd78829dba57a7ed8cbe4efab6731548fd
unknown <vitorsinedino@gmail.com>
2021-05-20 14:42:40 -0300

    added carrousel section

---
+++ src/sections/carrousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
+class Carrousel extends React.Component {
+    render() {
+        return (
+            <Container id="carrousel-1">
+                <Row>
+                   
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Carrousel;
